a54e41b4f7d4af9ce85d26ee761575d39a93fdcf
Cory Bass <cory.bass.07@gmail.com>
2021-04-22 12:41:55 -0500

    state example finished usersearch started

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import ReactDOM from 'react-dom';
 
+// local files
+import GuestList from './state/GuestList'
+
 const App = () => {
     return <div>
-        <h1>Hi there!</h1>
+        <GuestList />
     </div>
 }
 
